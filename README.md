b0f3cb246545036217b06360f1333cfe53be66c2
jiangmin <39113843+wangjiangmin@users.noreply.github.com>
2022-11-21 16:38:04 +0000

    ok

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # java-demo
+尝试使用在线的vs code编写一些java代码
---
+++ src/com.wjm.demo/HelloVscode.java
@@ -0,0 +1,7 @@
+import com.wjm.demo;
+
+class HelloVscode {
+    public static void main(String[] args) {
+        System.out.println("hello vscode");
+    }
+}
